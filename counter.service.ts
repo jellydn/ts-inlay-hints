f8d2d311ed494553b3372c01eb01706c876f74ed
Huynh Duc Dung <dunghd.it@gmail.com>
2023-07-09 18:40:36 +0800

    feat: add enum usage for counter service

--- counter.service.ts
+++ counter.service.ts
@@ -1,6 +1,11 @@
 import type { FastifyInstance } from "fastify";
 import S from "fluent-json-schema";
 
+enum CounterAction {
+  INCREASE = "increase",
+  DECREASE = "decrease"
+}
+
 export function counterService(app: FastifyInstance) {
   let counter = 0;
   app.get("/counter", {}, async (_request, _response) => ({ counter }));
@@ -8,7 +13,7 @@ export function counterService(app: FastifyInstance) {
   const schema = {
     querystring: S.object().prop(
       "type",
-      S.string().enum(["increase", "decrease"]).required()
+      S.string().enum([CounterAction.DECREASE, CounterAction.INCREASE]).required()
     ),
   };
   app.post<{
@@ -21,7 +26,7 @@ export function counterService(app: FastifyInstance) {
       schema,
     },
     async (request, _reply) => {
-      if (request.query.type === "increase") {
+      if (request.query.type === CounterAction.INCREASE) {
         counter++;
       } else {
         counter--;
