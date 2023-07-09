e16d1eba83d867edeefd10f2c1d5fca4b6af8d76
Huynh Duc Dung <dunghd.it@gmail.com>
2023-07-09 11:03:28 +0800

    feat: add swagger ui

--- app.ts
+++ app.ts
@@ -1,7 +1,37 @@
 import fastify from 'fastify';
+import fastifySwagger from '@fastify/swagger';
+import fastifySwaggerUI from '@fastify/swagger-ui';
+
 import { counterService } from './counter.service';
 
 const app = fastify({ logger: true });
+
+app.register(fastifySwagger, {
+  swagger: {
+    info: {
+      title: 'Counter API',
+      description: 'Counter API Documentation',
+      version: '0.1.0'
+    },
+    externalDocs: {
+      url: 'https://swagger.io',
+      description: 'Find more info here'
+    },
+    host: 'localhost:3000',
+    schemes: ['http'],
+    consumes: ['application/json'],
+    produces: ['application/json']
+  },
+});
+await app.register(fastifySwaggerUI, {
+  routePrefix: '/docs',
+  uiConfig: {
+    docExpansion: 'full',
+    deepLinking: false
+  },
+  staticCSP: true,
+  transformSpecificationClone: true
+})
 
 // Register your service
 counterService(app);
